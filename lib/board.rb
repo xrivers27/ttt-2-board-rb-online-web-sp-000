e77b607ceca789892ba3ebd8776eaf6f2f9889a5
Lorin Xavier Rivers <xrivers27@gmail.com>
2020-12-07 22:13:55 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,2 @@
 # Define the variable board below.
-board = ["top left", "top middle", "top right", "middle left", "middle middle", "middle right", "bottom left", "bottom middle", "bottom right"]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
